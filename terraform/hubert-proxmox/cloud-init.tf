822150e6e6db6d00dc625041fbbb3b198df384d5
Dawo9889 <dawid.spolecznosci@gmail.com>
2025-10-17 19:23:13 +0200

    Add Terraform configuration files for Proxmox setup and VM definitions

---
+++ terraform/hubert-proxmox/cloud-init.tf
@@ -0,0 +1,60 @@
+resource "proxmox_vm_qemu" "cloud-init" {
+  for_each = var.vm_configs
+
+  vmid = each.value.vm_id
+  name = each.value.name
+  target_node = "pve1"
+
+  clone = "ubuntu-server-noble"
+  full_clone = false
+  scsihw = "virtio-scsi-single"
+
+  os_type = "cloud-init"
+  memory = each.value.memory
+
+  onboot = each.value.onboot
+  startup = each.value.startup
+
+  ipconfig0 = each.value.ipconfig
+  ciuser = each.value.ciuser
+  cipassword = each.value.cipassword
+  sshkeys = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILhKpbWy7VPP7p7CL0/DdNtrWHnTTogaAfXmNegECbJZ For automation"
+  nameserver = "1.1.1.1"
+
+  serial {
+    id = 0
+    type = "socket"
+  }
+
+  cpu {
+    cores = each.value.cores
+  }
+
+  network {
+    id = 0
+    model = "virtio"
+    bridge = each.value.bridge
+    firewall = true
+    tag = each.value.network_tag
+  }
+
+  disks {
+    virtio {
+      virtio0 {
+        disk {
+          storage   = "local-lvm"
+          size      = "32G"
+          iothread  = true
+          replicate = false
+        }
+      }
+    }
+    ide {
+      ide0 {
+        cloudinit {
+          storage = "local-lvm"
+        }
+      }
+    }
+  }
+}
---
+++ terraform/hubert-proxmox/variables.tf
@@ -0,0 +1,142 @@
+variable proxmox_api_url {
+  type = string
+}
+
+variable proxmox_api_token_id {
+  type = string
+}
+
+variable proxmox_api_token {
+  type = string
+}
+
+  variable "vm_configs" {
+    type = map(object({
+      vm_id       = number
+      name        = string
+      memory      = number
+      vm_state    = string
+      onboot      = bool
+      startup     = string
+      ipconfig    = string
+      ciuser      = string
+      cipassword  = string
+      cores       = number
+      bridge      = string
+      network_tag = number
+    }))
+    default = {
+      "master-node1" = {
+        vm_id       = 1000
+        name        = "master-1"
+        memory      = 4096
+        vm_state    = "stopped"
+        onboot      = true
+        startup     = "order=1"
+        ipconfig    = "ip=192.168.4.150/24,gw=192.168.4.1"
+        ciuser      = "root"
+        cipassword  = "zaq12wsx"
+        cores       = 2
+        bridge      = "vmbr0"
+        network_tag = 0
+      },
+      "master-node2" = {
+        vm_id       = 1001
+        name        = "master-2"
+        memory      = 4096
+        vm_state    = "stopped"
+        onboot      = true
+        startup     = "order=1"
+        ipconfig    = "ip=192.168.4.151/24,gw=192.168.4.1"
+        ciuser      = "root"
+        cipassword  = "zaq12wsx"
+        cores       = 2
+        bridge      = "vmbr0"
+        network_tag = 0
+      },
+      "master-node3" = {
+        vm_id       = 1002
+        name        = "master-3"
+        memory      = 2048
+        vm_state    = "stopped"
+        onboot      = true
+        startup     = "order=1"
+        ipconfig    = "ip=192.168.4.152/24,gw=192.168.4.1"
+        ciuser      = "root"
+        cipassword  = "zaq12wsx"
+        cores       = 2
+        bridge      = "vmbr0"
+        network_tag = 0
+      },
+      "worker-node1" = {
+        vm_id       = 1003
+        name        = "worker-1"
+        memory      = 1024
+        vm_state    = "stopped"
+        onboot      = true
+        startup     = "order=1"
+        ipconfig    = "ip=192.168.4.153/24,gw=192.168.4.1"
+        ciuser      = "root"
+        cipassword  = "zaq12wsx"
+        cores       = 1
+        bridge      = "vmbr0"
+        network_tag = 0
+      },
+      "worker-node2" = {
+        vm_id       = 1004
+        name        = "worker-2"
+        memory      = 1024
+        vm_state    = "stopped"
+        onboot      = true
+        startup     = "order=1"
+        ipconfig    = "ip=192.168.4.154/24,gw=192.168.4.1"
+        ciuser      = "root"
+        cipassword  = "zaq12wsx"
+        cores       = 1
+        bridge      = "vmbr0"
+        network_tag = 0
+      },
+      "lb-node1" = {
+        vm_id       = 1005
+        name        = "lb-1"
+        memory      = 512
+        vm_state    = "stopped"
+        onboot      = true
+        startup     = "order=1"
+        ipconfig    = "ip=192.168.4.155/24,gw=192.168.4.1"
+        ciuser      = "root"
+        cipassword  = "zaq12wsx"
+        cores       = 1
+        bridge      = "vmbr0"
+        network_tag = 0
+      },
+      "lb-node2" = {
+        vm_id       = 1006
+        name        = "lb-2"
+        memory      = 512
+        vm_state    = "stopped"
+        onboot      = true
+        startup     = "order=1"
+        ipconfig    = "ip=192.168.4.156/24,gw=192.168.4.1"
+        ciuser      = "root"
+        cipassword  = "zaq12wsx"
+        cores       = 1
+        bridge      = "vmbr0"
+        network_tag = 0
+      },
+      "nfs-server" = {
+        vm_id = 1007
+        name = "nfs-server"
+        memory      = 512
+        vm_state    = "stopped"
+        onboot      = true
+        startup     = "order=1"
+        ipconfig    = "ip=192.168.4.157/24,gw=192.168.4.1"
+        ciuser      = "root"
+        cipassword  = "zaq12wsx"
+        cores       = 1
+        bridge      = "vmbr0"
+        network_tag = 0
+      }
+    }
+  }
